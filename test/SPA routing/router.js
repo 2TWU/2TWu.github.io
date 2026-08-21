f0c2aa633250f2732328acb4dc99db76e0b6e44e
2TWU <hiedge666@gmail.com>
2026-08-21 17:51:49 +0800

    test routing complete ;;

--- test/SPA routing/router.js
+++ test/SPA routing/router.js
@@ -5,38 +5,44 @@ document.addEventListener("DOMContentLoaded",()=>{
   // 要注入的元素
   const contentInject = document.getElementById("content") ;
 
-
   // Routing table 路由表
   // --------------
   const routes = {
-    "/" : "./src/pages/home.html",
-    "/about" : "./src/pages/about.html",
-    "/post" : "./src/pages/post.html"
+    "" : "./src/pages/home.html",
+    "about" : "./src/pages/about.html",
+    "post" : "./src/pages/post.html"
   }
 
   // 設定路由根節點( 被注入內容的元素 )
   var Routing_root = "./index.html" ;
-
+  
   // 偵測 URL 變化
+  // 當 hashchange 發生時觸發將網址變化傳入 loadPage 函式
   window.addEventListener("hashchange",()=>{
-    console.log("url change")
+    console.log("url change to " + window.location.hash)
     loadPage(window.location.hash);
   })
+
+
   // 切換 content 內容
+  // 
   function loadPage(hash){
     const page = hash.replace("#","");
-    console.log(page);
-    console.log(routes[page]);
-    // fetch(routes[page])
-    //   .then(response => response.text())
-    //   .then(html => {
-    //     contentInject.innerHTML = html ;
-    //   }
-    // );
+    console.log("current page is " + page);
+    console.log(routes[page] , "is loading");
+    fetch(routes[page])
+      .then(response => response.text())
+      .then(html => {
+        contentInject.innerHTML = html ;
+      }
+    );
 
-    contentInject.innerHTML = routes[page] || routes["/"];
+    // contentInject.innerHTML = routes[page] || routes["/"];
   }
 
+
+  // 初始化，預設先讀 home.html
+  loadPage(window.location.hash);
 
 
 
@@ -45,10 +51,10 @@ document.addEventListener("DOMContentLoaded",()=>{
   var value = Object.values(routes);
   var skeys = "/"
 
-  console.log(value);
+  // console.log(value);
 
-  console.log("value is "+routes["/"]);
+  // console.log("value is "+routes["/"]);
 
-  console.log("key is "+skeys+" , value is "+routes[skeys]);
+  // console.log("key is "+skeys+" , value is "+routes[skeys]);
 })
 
